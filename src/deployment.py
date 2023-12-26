1d59dc498fa99d80e4863fa11483442d94202147
Nuo Chen <chennuo909@163.com>
2023-12-26 21:08:16 +0800

    Update deployment.py

--- src/deployment.py
+++ src/deployment.py
@@ -6,22 +6,30 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 app = FastAPI()
-model_path = "/home/cn/root/ckpt/ckpt"
 
 
 def load_model(model_path):
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForCausalLM.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(
+        "ckpt", trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(
+        "ckpt", trust_remote_code=True).cuda()
     return tokenizer, model
 
 
 tokenizer, model = load_model(model_path)
 @app.get('/predict')
 async def predict(input_text, max_length=256, top_p=0.6, num_return_sequences=5):
-    #inputs = tokenizer(input_text, return_tensors='pt')
-    inputs = tokenizer.apply_chat_template(input_text, return_tensors="pt").to(model.device)
-    return model.generate(inputs, max_new_tokens=512, do_sample=False, top_k=50, top_p=0.95, num_return_sequences=1, 
-    eos_token_id=32021, pad_token_id=32021)
+    prompt = input_text
+    conversation=[]
+    conversation.append({'role': 'user', 'content': prompt})
+    print("conversation")
+    inputs = tokenizer.apply_chat_template(
+        conversation, return_tensors="pt").to(model.device)
+    outputs = model.generate(inputs, max_new_tokens=512, do_sample=False, top_k=50, top_p=0.95, num_return_sequences=1,
+                             eos_token_id=32021, pad_token_id=32021)
+    response=tokenizer.decode(outputs[0][len(inputs[0]):], skip_special_tokens=True)
+
+    return response
 
 if __name__ == '__main__':
     #　在调试的时候开源加入一个reload=True的参数，正式启动的时候可以去掉
